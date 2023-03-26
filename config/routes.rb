61de40e5377cc32cf23fd16cf5eaf6b12e34ed6c
KAZY9 <gi09ty01@gmail.com>
2023-03-26 13:34:11 +0900

    Made signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'pages#index'
+  # get 'pages#show'
+  get '/pages/show', to: 'pages#show'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
