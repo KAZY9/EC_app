c7784e586094bf31a4a3863cfdc3a68be6a95e74
KAZY9 <gi09ty01@gmail.com>
2023-03-27 13:39:17 +0900

    Made constants.rb and modified new.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def constants
+    @job_list = Constants.job_list
+  end
+
   def after_sign_in_path_for(resource)
     pages_show_path
   end
