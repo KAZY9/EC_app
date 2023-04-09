325faa1bb33b448a0cd8d0bcb15c38b78abb01e7
KAZY9 <gi09ty01@gmail.com>
2023-04-09 21:49:41 +0900

    birthday.check.jsを修正

--- app/javascript/birthday.check.js
+++ app/javascript/birthday.check.js
@@ -1,17 +1,21 @@
 document.addEventListener("turbo:load", function() {
   // DOM 要素を取得
-const yearSelectElm = document.querySelector('[name="user[birthday(1i)]"]');
-const monthSelectElm = document.querySelector('[name="user[birthday(2i)]"]');
-const daySelectElm = document.querySelector('[name="user[birthday(3i)]"]');
+  const yearSelectElm = document.querySelector('[name="user[birthday(1i)]"]');
+  const monthSelectElm = document.querySelector('[name="user[birthday(2i)]"]');
+  const daySelectElm = document.querySelector('[name="user[birthday(3i)]"]');
 
-// 日付を更新する関数
-function updateDaySelect() {
-  const month = parseInt(monthSelectElm.value, 10);
-  const year = parseInt(yearSelectElm.value, 10);
-
-  if (!isNaN(month) && !isNaN(year)) {
+  // 日付を更新する関数
+  function updateDaySelect() {
+    const month = parseInt(monthSelectElm.value, 10);
+    const year = parseInt(yearSelectElm.value, 10);
+    const selectedDay = document.querySelector('#user_birthday_3i [selected="selected"]');
+  
+    if (isNaN(month) || isNaN(year)) {
+      return;
+    }
+  
     const daysInMonth = new Date(year, month, 0).getDate();
-
+  
     // 選択肢を更新
     for (let i = daySelectElm.options.length - 1; i >= daysInMonth; i--) {
       daySelectElm.remove(i);
@@ -22,15 +26,23 @@ function updateDaySelect() {
       option.text = i;
       daySelectElm.add(option);
     }
+  
+    if (selectedDay) {
+      for (let i = 1; i < daySelectElm.options.length; i++ ) {
+        if (selectedDay.value == daySelectElm.options[i].value) {
+          daySelectElm.options[i].setAttribute('selected', 'selected');
+        }
+      }
+    }
   }
-}
+  
 
-// DOM 要素が存在する場合、日付を更新する
-if (yearSelectElm && monthSelectElm && daySelectElm) {
-  yearSelectElm.addEventListener('change', updateDaySelect);
-  monthSelectElm.addEventListener('change', updateDaySelect);
-  updateDaySelect();
-}
+  // DOM 要素が存在する場合、日付を更新する
+  if (yearSelectElm && monthSelectElm && daySelectElm) {
+    yearSelectElm.addEventListener('change', updateDaySelect);
+    monthSelectElm.addEventListener('change', updateDaySelect);
+    updateDaySelect();
+  }
 });
 
 
