e5fc85fc0d11b876143175a84eeb48933d639015
KAZY9 <gi09ty01@gmail.com>
2023-04-23 21:28:46 +0900

    cards_controller.rb、card.jsを修正

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,17 +1,17 @@
 class CardsController < ApplicationController
   before_action :sign_in_required, only: [:new, :create, :destroy]
+  before_action :init_payjp, only: [:new, :create, :destroy]
 
   def new
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    card = Card.find_by(user_id: current_user.id) # cardsテーブルからユーザーのカード情報を取得
-    if card.present?
-      customer = Payjp::Customer.retrieve(card.customer_id) # 顧客idを元に、顧客情報を取得
-      @cards = customer.cards.all
+    cards = Card.where(user_id: current_user.id)
+    @cards = []
+    cards.each do |card|
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @cards.concat(customer.cards.data)
     end
   end
 
   def create
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     customer = Payjp::Customer.create(
       description: 'test', 
       card: params[:token_id]
@@ -28,14 +28,17 @@ class CardsController < ApplicationController
 
   def destroy
     card = Card.find_by(user_id: current_user.id)
-    if card.blank?
-    else
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    unless card.blank?
       customer = Payjp::Customer.retrieve(card.customer_id)
       if customer.delete && card.delete
         redirect_to new_card_path, status: :see_other
       end
     end
   end
- 
+
+  private 
+  
+  def init_payjp
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+  end
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,48 +1,42 @@
-// $(document).on("turbo:load", function(){ 
 var payjp = Payjp('pk_test_6e12b67dda6c33c97a8b391a');
 document.addEventListener("turbo:load", function() {
     if (document.URL.match('mypage/cards/')) {
     
-    var elements = payjp.elements();
+        var elements = payjp.elements();
 
-    var numberElement = elements.create('cardNumber', {placeholder: ""});
-    var expiryElement = elements.create('cardExpiry', {placeholder: "月/年"});
-    var cvcElement = elements.create('cardCvc', {placeholder: "例：123"});
-      
-    // elementをDOM上に配置
-    numberElement.mount('#number-form');
-    expiryElement.mount('#expiry-form');
-    cvcElement.mount('#cvc-form');
+        var numberElement = elements.create('cardNumber', {placeholder: ""});
+        var expiryElement = elements.create('cardExpiry', {placeholder: "月/年"});
+        var cvcElement = elements.create('cardCvc', {placeholder: "例：123"});
+        
+        // elementをDOM上に配置
+        numberElement.mount('#number-form');
+        expiryElement.mount('#expiry-form');
+        cvcElement.mount('#cvc-form');
 
+        $("#info_submit").click(function (e) {
+            e.preventDefault();
 
-    $("#info_submit").click(function (e) {
-        e.preventDefault();
-
-        var cardholderName = $("#card_form #cardholder_name").val();
-        console.log(cardholderName);
-        //ここでトークンを作成　
-        // createTokenの引数には任意のElement1つを渡す
-        payjp.createToken(numberElement, {card: {name: cardholderName}}).then(function (r) {
-          if (r.error) {  //  通信に失敗したとき
-            alert(r.error.message)
-            regist_card.prop('disabled', false)
-          } else {
-            alert("登録が完了しました");
-            $("#card_token").append(
-              `<input type="hidden" name="token_id" value=${r.id}>
-              <input type="hidden" name="card_token" value=${r.card.id}>`
-            );
-            $('#card_form')[0].submit();
-            //  ↑↑　ここでtype='hidden'にしてsubmitにtokenを乗せている
-  
-            $("#card_number").removeAttr("name");
-            $("#cvc-from").removeAttr("name");
-            $("#exp_month").removeAttr("name");
-            $("#exp_year").removeAttr("name");
-            //  ↑↑　ここでremoveAttrで記述を削除してます
-          };
-        });
-    }); 
-  }
+            var cardholderName = $("#card_form #cardholder_name").val();
+        
+            payjp.createToken(numberElement, {card: {name: cardholderName}}).then(function (r) {
+            if (r.error) {  //  通信に失敗したとき
+                alert(r.error.message)
+                regist_card.prop('disabled', false)
+            } else {
+                alert("登録が完了しました");
+                $("#card_token").append(
+                `<input type="hidden" name="token_id" value=${r.id}>
+                <input type="hidden" name="card_token" value=${r.card.id}>`
+                );
+                $('#card_form')[0].submit();
+    
+                $("#card_number").removeAttr("name");
+                $("#cvc-from").removeAttr("name");
+                $("#exp_month").removeAttr("name");
+                $("#exp_year").removeAttr("name");
+            };
+            });
+        }); 
+    }
 });
 
