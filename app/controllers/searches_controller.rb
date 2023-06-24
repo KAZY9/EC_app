1e75b35c17280546a8d8957f980a81c8ce7fc639
KAZY9 <gi09ty01@gmail.com>
2023-06-25 00:29:36 +0900

    searches_controller.rb、_filter_search.html.erb、filter_search.html.erbをリファクタリング

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,24 +9,17 @@ class SearchesController < ApplicationController
     end
 
     def filter_search
-        @word = params[:word]
-        @brand = params[:brand]
-        @color = params[:color]
-        @shape = params[:shape]
-        @carrying_style = params[:carrying_style]
-        @style = params[:style]
-        @closure_method = params[:closure_method]
-        if @word.blank? && @brand.blank? && @color.blank? && @shape.blank? && @carrying_style.blank? && @style.blank? && @closure_method.blank?
+        if params[:word].blank? && params[:brand].blank? && params[:color].blank? && params[:shape].blank? && params[:carrying_style].blank? && params[:style].blank? && params[:closure_method].blank?
             @products = Product.all.page(params[:page]).per(12)
         else
             @products = Product.all
-            @products = @products.where(brand: @brand).page(params[:page]).per(12) if @brand.present?
-            @products = @products.where(color: @color).page(params[:page]).per(12) if @color.present?
-            @products = @products.where(shape: @shape).page(params[:page]).per(12) if @shape.present?
-            @products = @products.where(carrying_style: @carrying_style).page(params[:page]).per(12) if @carrying_style.present?
-            @products = @products.where(style: @style).page(params[:page]).per(12) if @style.present?
-            @products = @products.where(closure_method: @closure_method).page(params[:page]).per(12) if @closure_method.present?
-            @products = @products.looks(@word).page(params[:page]).per(12) if @word.present?
+            @products = @products.where(brand: params[:brand]).page(params[:page]).per(12) if params[:brand].present?
+            @products = @products.where(color: params[:color]).page(params[:page]).per(12) if params[:color].present?
+            @products = @products.where(shape: params[:shape]).page(params[:page]).per(12) if params[:shape].present?
+            @products = @products.where(carrying_style: params[:carrying_style]).page(params[:page]).per(12) if params[:carrying_style].present?
+            @products = @products.where(style: params[:style]).page(params[:page]).per(12) if params[:style].present?
+            @products = @products.where(closure_method: params[:closure_method]).page(params[:page]).per(12) if params[:closure_method].present?
+            @products = @products.looks(params[:word]).page(params[:page]).per(12) if params[:word].present?
         end
     end
 end
--- app/javascript/filter_search.js
+++ app/javascript/filter_search.js
@@ -1,43 +1,27 @@
-$(document).on("turbo:load", function () {
-    var $brandToggle = $('.brand-toggle');
-    var $searchBrand = $('.search-brand');
-    var $colorToggle = $('.color-toggle');
-    var $searchColor = $('.search-color');
-    var $shapeToggle = $('.shape-toggle');
-    var $searchShape = $('.search-shape');
-    var $carrying_styleToggle = $('.carrying_style-toggle');
-    var $searchCarrying_style = $('.search-carrying_style');
-    var $styleToggle = $('.style-toggle');
-    var $searchStyle = $('.search-style');
-    var $closure_methodToggle = $('.closure_method-toggle');
-    var $searchClosure_method = $('.search-closure_method');
-  
-    $brandToggle.click(function() {
-      $searchBrand.slideToggle();
+$(document).on("turbo:load", function () {  
+
+    $('.brand-toggle').click(function() {
+        $('.search-brand').slideToggle();
     });
 
-    $colorToggle.click(function() {
-        $searchColor.slideToggle();
+    $('.color-toggle').click(function() {
+        $('.search-color').slideToggle();
     });
 
-    $shapeToggle.click(function() {
-        $searchShape.slideToggle();
+    $('.shape-toggle').click(function() {
+        $('.search-shape').slideToggle();
     });
     
-    $carrying_styleToggle.click(function() {
-        $searchCarrying_style.slideToggle();
+    $('.carrying_style-toggle').click(function() {
+        $('.search-carrying_style').slideToggle();
     });
 
-    $styleToggle.click(function() {
-        $searchStyle.slideToggle();
+    $('.style-toggle').click(function() {
+        $('.search-style').slideToggle();
     });
 
-    $closure_methodToggle.click(function() {
-        $searchClosure_method.slideToggle();
-    });
-
-    $('.filter-search-btn').click(function(event) {
-        event.stopPropagation();
+    $('.closure_method-toggle').click(function() {
+        $('.search-closure_method').slideToggle();
     });
   });
 
